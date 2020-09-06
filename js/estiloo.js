1d51311bff43b7a47582c7b7bf33b8f7bc6ac586
Chiara Rossi <chiaa_rossi@hotmail.com>
2020-09-06 14:26:52 -0300

    Cambio logo

---
+++ js/estiloo.js
@@ -0,0 +1,21 @@
+// selector
+var menu = document.querySelector('.hamburger');
+
+// method
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".menuppal" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
+
+//Solución con jQUery
+/*$(document).ready(function(){
+	$('.hamburger').click(function() {
+		$('.hamburger').toggleClass('is-active');
+		$('.menuresponsive').toggleClass('is-active');
+		return false;
+	});
+});*/
